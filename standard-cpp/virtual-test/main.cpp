1811129856233f65298ad43744b369fdb9e130f5
ktae82 <aroma0403@naver.com>
2022-11-29 20:41:25 +0000

    modified virtual test

--- standard-cpp/virtual-test/main.cpp
+++ standard-cpp/virtual-test/main.cpp
@@ -14,27 +14,49 @@ public:
   void function() override { std::cout << "\tChild::function()" << std::endl; }
 };
 
+void createByUniquePtr() {
+  std::cout << __FUNCTION__ << "() entered" << std::endl;
+  {
+    auto spChild = std::make_unique<Child>();
+    spChild->function();
+  }
+  std::cout << __FUNCTION__ << "() exited" << std::endl;
+}
+
 void createBySharedPtr() {
-  std::cout << "call " << __FUNCTION__ << "() entered" << std::endl;
+  std::cout << __FUNCTION__ << "() entered" << std::endl;
   {
     auto spChild = std::make_shared<Child>();
     spChild->function();
   }
-  std::cout << "call " << __FUNCTION__ << "() exited" << std::endl;
+  std::cout << __FUNCTION__ << "() exited" << std::endl;
 }
 
 void createByRawPtr() {
-  std::cout << "call " << __FUNCTION__ << "() entered" << std::endl;
+  std::cout << __FUNCTION__ << "() entered" << std::endl;
   {
     VirtualParent *pChild = new Child();
     pChild->function();
     delete pChild;
   }
-  std::cout << "call " << __FUNCTION__ << "() exited" << std::endl;
+  std::cout << __FUNCTION__ << "() exited" << std::endl;
+}
+
+void createByRawPtrToSharedPtr() {
+  std::cout << __FUNCTION__ << "() entered" << std::endl;
+  {
+    std::shared_ptr<VirtualParent> spChild{new Child};
+    spChild->function();
+  }
+  std::cout << __FUNCTION__ << "() exited" << std::endl;
 }
 
 int main() {
+  createByUniquePtr();
+
   createBySharedPtr();
 
   createByRawPtr();
+
+  createByRawPtrToSharedPtr();
 }
