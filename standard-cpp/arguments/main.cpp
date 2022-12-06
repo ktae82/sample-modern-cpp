8e90767cedb2bb67b7a60bdd1d7219f07a6760ae
ktae82 <aroma0403@naver.com>
2022-12-06 10:36:42 +0000

    added arguments sample

---
+++ standard-cpp/arguments/main.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+void toStringVector(int argc, char *argv[], std::vector<std::string> &out) {
+  std::cout << __FUNCTION__ << "(), argc:" << argc << std::endl;
+
+  // change argc,argv to std::vector<std::string>
+  out = std::move(std::vector<std::string>(argv, argv + argc));
+
+  // print
+  for (const auto &elem : out) {
+    std::cout << elem << " " << std::flush;
+  }
+  std::cout << std::endl;
+}
+
+void toCharPtrVector(std::vector<std::string> &in,
+                     std::vector<const char *> &out) {
+  std::cout << __FUNCTION__ << "(), in.size():" << in.size() << std::endl;
+
+  // change std::vector<std::string> to std::vector<char>
+  out.resize(in.size());
+  std::transform(in.begin(), in.end(), out.begin(),
+                 [](std::string &str) { return str.c_str(); });
+
+  // print
+  for (const auto &elem : in) {
+    std::cout << elem << " " << std::flush;
+  }
+  std::cout << std::endl;
+}
+
+void printArgcArgv(int argc, char *argv[]) {
+  std::cout << __FUNCTION__ << "(), argc:" << argc << std::endl;
+
+  // print
+  for (int i = 0; i < argc; i++) {
+    std::cout << argv[i] << " " << std::flush;
+  }
+  std::cout << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+  std::cout << __FUNCTION__ << "(), argc:" << argc << std::endl;
+
+  std::vector<std::string> argVecStr;
+  std::vector<const char *> argVecCharPtr;
+
+  toStringVector(argc, argv, argVecStr);
+  toCharPtrVector(argVecStr, argVecCharPtr);
+  printArgcArgv(argVecCharPtr.size(),
+                const_cast<char **>(argVecCharPtr.data()));
+
+  return 0;
+}
